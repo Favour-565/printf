67b6a5f6dd043c35046da0b9083dc6ac90b60b81
chimaskyy <chiamakahj2@gmail.com>
2023-07-26 13:50:23 -0700

    Update printf file

--- my_printf.c
+++ my_printf.c
@@ -1,4 +1,34 @@
 #include "main.h"
+
+/**
+ * spec_check - Function to check for format specifires
+ * @n: Character to pass
+ * @arg_list: Arguements passed
+ * Return: Number of character printed
+ */
+
+int spec_check(char n, va_list arg_list)
+{
+	switch (n)
+	{
+		case 'c':
+			return (my_putchar(va_arg(arg_list, int)));
+		case 's':
+			return (my_putchar(va_arg(arg_list, char *)));
+		case '%':
+			return (my_putchar(va_arg(arg_list, int)));
+		case 'd':
+			return (int_print(va_arg(args_list, int)));
+		case 'i':
+			return (int_print(va_arg(args_list, int)));
+		case 'S':
+			return (put_S(va_arg(args_list, char *)));
+		default:
+			return (my_putchar('%') + my_putchar(n));
+	}
+}
+
+
 /**
  * _printf - Function that print string
  * @format: First argument passed
@@ -8,7 +38,6 @@ int _printf(const char *format, ...)
 {
 	unsigned int i = 0;
 	unsigned int re_val = 0;
-	int r_val;
 	va_list args_list;
 
 	va_start(args_list, format);
@@ -20,46 +49,10 @@ int _printf(const char *format, ...)
 		if (format[i] == '%' && format[i + 1] == '\0')
 			return (-1);
 		if (format[i] == '%')
-		{
-			if (format[i + 1] == 'c')
-			{
-				my_putchar(va_arg(args_list, int));
-				i++;
-
-			}
-			else if (format[i + 1] == 's')
-			{
-				r_val = put_str(va_arg(args_list, char*));
-				i++;
-				re_val += (r_val - 1);
-			}
-			else if (format[i + 1] == '%')
-			{
-				my_putchar('%');
-				i++;
-			}
-			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
-			{
-				re_val += int_print(va_arg(args_list, int));
-				i++;
-			}
-			else if (format[i + 1] == 'S')
-			{
-				put_S(va_arg(args_list, char *));
-				i++;
-				re_val += (r_val - 1);
-			}
-			else
-			{
-				my_putchar(format[i]);
-				my_putchar(format[i + 1]);
-				i++;
-				re_val += 2;
-			}
-		}
+			re_val += speck_check(format[++i], args_list);
 		else
 		{
-			my_putchar(va_arg(args_list, int));
+			my_putchar(va_arg(arg_list, int));
 			re_val += 1;
 		}
 	}
